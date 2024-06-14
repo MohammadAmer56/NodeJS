599609b89abc9818184cdd0f502d5d90dee9948e
MohammadAmer56 <147609037+MohammadAmer56@users.noreply.github.com>
2024-06-14 18:20:05 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,63 +1,36 @@
-const mongoose = require("mongoose"); // Importing mongoose
+const express = require("express");
+require("./config");
+const Product = require("./products");
 
-// Connecting to the MongoDB database
-mongoose.connect("mongodb://localhost:27017/e-comm");
+const app = express();
+app.use(express.json());
 
-// Defining the schema for the 'products' collection
-const ProductsSchema = new mongoose.Schema({
-  name: String,
-  price: Number,
-  brand: String,
-  category: String,
+app.post("/create", async (req, resp) => {
+  let data = new Product(req.body);
+  let result = await data.save();
+  console.log(result); //need to parse the body to get data from body for req for that you use app.use(express.json())
+  resp.send(result);
 });
 
-// Creating a model for the 'products' collection based on the defined schema
-const ProductsModel = mongoose.model("products", ProductsSchema);
+app.get("/list", async (req, resp) => {
+  let data = await Product.find();
+  resp.send(data);
+});
 
-const saveInDB = async () => {
-  // Creating a new product document with the defined schema
-  let data = new ProductsModel({
-    name: "Note Pro",
-    price: 250,
-    brand: "maxx",
-    category: "mobiles", // Including the 'category' field
-  });
+app.delete("/delete/:_id", async (req, resp) => {
+  let data = await Product.deleteOne(req.params);
+  resp.send(data);
+});
 
-  // Saving the new document to the database
-  let result = await data.save().catch((error) => {
-    // Handle any errors that occur during the save operation
-    console.error("Error saving document:", error);
-  });
+app.put("/update/:_id", async (req, resp) => {
+  let data = await Product.updateOne(
+    //  {} condition
+    req.params,
+    {
+      $set: req.body, //$set updated data
+    }
+  );
+  resp.send(data);
+});
 
-  // If the save operation was successful, 'result' will be defined
-  if (result) {
-    console.log("Document saved successfully:", result); // Log the saved document
-  }
-};
-
-const updateInDB = async () => {
-  const ProductsModel = mongoose.model("products", ProductsSchema);
-    let data = await ProductsModel.updateOne({name:"Z75"},{$set:{price:150}})
-    console.log(data)
-};
-
-
-// updateInDB();
-
-
-const deleteInDB = async () => {
-  const ProductsModel = mongoose.model("products", ProductsSchema);
-    let data = await ProductsModel.deleteMany({name:'Note Pro'})
-    console.log(data)
-}
-
-// deleteInDB();
-
-const findInDB = async () => {
-    const ProductsModel = mongoose.model("products", ProductsSchema);
-      let data = await ProductsModel.find({name: "mui"});
-      console.log(data)
-  }
-
-
-  findInDB();
+app.listen(4500);
